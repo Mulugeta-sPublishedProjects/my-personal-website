66de2a6f0e9590d61234b1f0cc1b5463b90ffee5
mulugeta <mulugeta.adamu97@gmail.com>
2024-11-06 23:25:27 +0300

    feat: feat

--- src/app/shared/menus.ts
+++ src/app/shared/menus.ts
@@ -3,6 +3,6 @@ export const menuItems = [
   { id: 2, href: "/skills", label: "Educations & Experiences" },
   { id: 3, href: "/services", label: "Services" },
   { id: 4, href: "/projects", label: "Projects" },
-  { id: 5, href: "/resume", label: "Resume" },
+  { id: 5, href: "/resume", label: "Resume/CV" },
   { id: 6, href: "/blogs", label: "Blog" },
 ];
