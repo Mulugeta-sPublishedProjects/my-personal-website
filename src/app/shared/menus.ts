6024c53856dabd1079873c65458e5c6386eba014
mulugeta <mulugeta.adamu97@gmail.com>
2024-11-11 17:11:48 +0300

    feat: fix

--- src/app/shared/menus.ts
+++ src/app/shared/menus.ts
@@ -2,7 +2,7 @@ export const menuItems = [
   { id: 1, href: "/", label: "Home" },
   { id: 2, href: "/skills", label: "Educations & Experiences" },
   { id: 4, href: "/projects", label: "Projects" },
-  { id: 5, href: "/resume", label: "CV/Educational Documents" },
+  { id: 5, href: "/resume", label: "CV/Documents" },
   { id: 6, href: "/testimonials", label: "Testimonials" },
   { id: 7, href: "/blogs", label: "Blog" },
   { id: 8, href: "/github", label: "My Github" },
